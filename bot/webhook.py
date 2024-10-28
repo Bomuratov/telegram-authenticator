5cf030f6f504733572fb820f7500fb5f0255f91d
Umidjon Bomurotov <umidjonbomurotov@Umidjon.local>
2024-10-29 03:10:43 +0500

    add update schema

--- bot/webhook.py
+++ bot/webhook.py
@@ -2,12 +2,17 @@ from fastapi import APIRouter
 from aiogram import types
 from config import settings
 from bot.commands import dp, bot
-
+from pydantic import BaseModel
+from typing import Optional
 
 router = APIRouter()
 
 
+class UpdateSchema(BaseModel):
+    update_id: Optional[int]
+    message: Optional[dict]
+
 @router.post(f"{settings.bot.path}{settings.bot.token}")
-async def bot_webhook(update: dict):
-    update = types.Update(**update)
+async def bot_webhook(update: UpdateSchema):
+    update = types.Update(**update.dict())
     await dp.feed_update(bot, update)
