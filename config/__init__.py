d2d5670a34fb04b769476a251b8bb920da1ac4a5
Bomuratov <antikko99@gmail.com>
2025-02-02 21:51:58 +0500

    deploy verificator on koyeb5

--- config/__init__.py
+++ config/__init__.py
@@ -1,8 +1,7 @@
 __all__ = (
     "settings",
-    "db_helper",
     "db_redis"
 )
 
 from config.settings import settings
-from config.db_helper import db_helper, db_redis
+from config.db_helper import db_redis
