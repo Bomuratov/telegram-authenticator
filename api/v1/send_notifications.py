bc5d6fb8817521bdb551f0e7ced916158f7fde10
Bomuratov <antikko99@gmail.com>
2025-03-24 15:58:42 +0500

    add functional notify order and accept or cancel order testeng on server fixing

--- api/v1/send_notifications.py
+++ api/v1/send_notifications.py
@@ -12,9 +12,9 @@ router = APIRouter()
 
 @router.post("/new-order/")
 async def new_order_notification(payload: Dict[str, Any]):
-    print(payload)
+
     restaurant_id = payload["restaurant"]
-    print(payload)
+
 
     keyboard = InlineKeyboardMarkup(
             inline_keyboard=[
@@ -35,3 +35,28 @@ async def new_order_notification(payload: Dict[str, Any]):
     except TelegramBadRequest as e:
         return {"detail": e}
     
+
+
+""""
+
+import { AccessTime, Close, DoneOutlined, Kitchen, Fastfood } from "@mui/icons-material";
+import { Chip } from "@mui/material";
+
+export const getStatusChip = (status: string) => {
+  switch (status) {
+    case "new":
+      return <Chip label="Новый" color="success" icon={<Fastfood />} />;
+    case "completed":
+      return <Chip label="Готово" color="success" icon={<DoneOutlined />} />;
+    case "pending":
+      return <Chip label="Ожидание" color="secondary" icon={<AccessTime />} />;
+    case "canceled":
+      return <Chip label="Отменено" color="error" icon={<Close />} />;
+    case "prepare":
+      return <Chip label="Готовиться" color="warning" icon={<Kitchen />} />;
+    default:
+      return <Chip label="Неизвестно" />;
+  }
+};
+
+"""
--- bot/order_callback.py
+++ bot/order_callback.py
@@ -1,3 +1,4 @@
+import asyncio
 import requests
 from fastapi import BackgroundTasks
 from aiogram import Router, types
@@ -6,17 +7,19 @@ from aiogram import Router, types
 bot_router = Router()
 
 
-def send_order_update(order_id: str, status: str):
-
+async def send_order_update(order_id: str, status: str):
     """Фоновая задача для отправки PUT-запроса на backend"""
-
-    url = f"https://backend.aurora-app.uz/api/orders/update/{order_id}"
+    url = f"https://backend.aurora-app.uz/api/orders/update/{order_id}/"
     headers = {'Content-Type': 'application/json'}
     data = {"status": status}
 
+    loop = asyncio.get_running_loop()
     try:
-        response = requests.put(url, json=data, headers=headers)
-        print(f"✅ Ответ от сервера ({status}):", response.status_code, response.text)
+        response = await loop.run_in_executor(
+            None, 
+            lambda: requests.put(url, json=data, headers=headers)
+            )
+        print(f"✅ Ответ от сервера ({status}):")
     except Exception as e:
         print(f"❌ Ошибка при обновлении заказа #{order_id}: {e}")
 
@@ -24,12 +27,13 @@ def send_order_update(order_id: str, status: str):
 @bot_router.callback_query(
     lambda c: c.data.startswith("accept_order") or c.data.startswith("reject_order")
 )
-async def handle_order_action(callback_query: types.CallbackQuery, background_tasks: BackgroundTasks):
+async def handle_order_action(callback_query: types.CallbackQuery):
 
-    action, order_id = callback_query.data.split(":", 6)
+    action, order_id = callback_query.data.split(":", 3)
 
     if action == "accept_order":
         text = f"✅ Заказ #{order_id} принял пользователь @{callback_query.from_user.username}"
+        
         status = "prepare"
     elif action == "reject_order":
         text = f"❌ Заказ #{order_id} отменил пользователь @{callback_query.from_user.username}"
@@ -37,8 +41,7 @@ async def handle_order_action(callback_query: types.CallbackQuery, background_ta
     else:
         await callback_query.answer("Ошибка! Неизвестное действие.")
         return
-    
+
     await callback_query.message.reply(text)
     await callback_query.message.delete_reply_markup()
-
-    background_tasks.add_task(send_order_update, order_id, status)
+    asyncio.create_task(send_order_update(order_id, status))
