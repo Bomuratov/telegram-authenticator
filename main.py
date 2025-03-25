125d36afaed401f4bd7d0a0e48cf0aa4ec14a1ce
Bomuratov <antikko99@gmail.com>
2025-03-25 13:34:23 +0500

    add functional notify order and accept or cancel order testeng on server fixing v14

--- main.py
+++ main.py
@@ -14,20 +14,15 @@ logger = logging.getLogger(__name__)
 
 origins = ["*"]
 
-async def keep_alive():
-    """Фоновая задача, чтобы Render не останавливал процесс"""
-    while True:
-        await asyncio.sleep(10)
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     url1 = settings.bot.url+settings.bot.path+settings.bot.token
-    # print(url1)
-    # await bot.set_webhook(f"{settings.bot.url}{settings.bot.path}{settings.bot.token}")
+    await bot.set_webhook(f"{settings.bot.url}{settings.bot.path}{settings.bot.token}")
     logger.info(f"Вебхук успешно установлен {await bot.get_webhook_info()}")
     task = asyncio.create_task(keep_alive())
     yield
-    task.cancel()
     logger.info("Заканчиваем работу")
     await bot.session.close()
     logger.info(f"Сессия бота закрыт: ")
@@ -51,12 +46,4 @@ fapp.add_middleware(
 
 @fapp.get("/health")
 async def health_check():
-    # webhook_info = await bot.get_webhook_info()
-    # webhook_url = f"{settings.bot.url}{settings.bot.path}{settings.bot.token}"
-
-    # if webhook_info.url != webhook_url:  # Устанавливаем вебхук, только если он не совпадает
-    #     await bot.set_webhook(webhook_url)
-    #     logger.info(f"✅ Вебхук установлен: {await bot.get_webhook_info()}")
-    # else:
-    #     logger.info(f"✅ Вебхук уже установлен: {webhook_info.url}")
     return {"status": "ok"}
